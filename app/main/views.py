54bfb20f0fa41c95670bb39e02689036d5223d02
xiaoqiangcoding <xiaoqiangcoding@users.noreply.github.com>
2016-03-16 09:05:40 +0800

    3.16 加了拓扑图数据

--- app/main/views.py
+++ app/main/views.py
@@ -11,7 +11,12 @@ from . import main
 from ..models import User
 from collect.monitor import get_tenant_token, get_tenants, get_tenant_instances, get_tenant_instance, get_tenant_resources,\
                             get_tenant_instance_meter, get_tenant_limits, get_user_token, get_tenant_flavors, \
-                            get_tenant_networks, get_tenant_subnets, get_tenant_routers
+                            get_tenant_networks, get_tenant_subnets, get_tenant_routers,get_one_firewalls_info,get_all_rules, \
+                            get_all_policies, get_all_firewalls_info, get_floatingips, get_security_groups,get_port_all_info,get_all_policies, \
+                            get_all_rules,get_list_meter_minute,get_instance_network_resource_id,get_meter_func_data,get_tuopu_info
+
+
+
 
 
 
@@ -103,3 +108,98 @@ def front_get_tenant_vm_meters(instance_id, meter_name):
     token = json.loads(request.args.get('token'))
     meter_json = get_tenant_instance_meter(token['id'], instance_id, meter_name)
     return json.dumps(meter_json)
+
+
+@main.route('/floatingips')
+def get_floatingips_info():
+    token = json.loads(request.args.get('token'))
+    floatingip_json = get_floatingips(token["id"])
+    return json.dumps(floatingip_json)
+
+
+@main.route('/security_groups')
+def get_security_groups_info():
+    token = json.loads(request.args.get('token'))
+    security_groups_json = get_security_groups(token["id"])
+    return json.dumps(security_groups_json)
+
+
+@main.route('/firewalls')
+def get_all_firewall():
+    token = json.loads(request.args.get('token'))
+    firewall_json = get_all_firewalls_info(token['id'])
+    return json.dumps(firewall_json)
+
+
+@main.route('/firewall_rules')
+def get_all_rule():
+    token = json.loads(request.args.get('token'))
+    rule_json = get_all_rules(token['id'])
+    return json.dumps(rule_json)
+
+
+@main.route('/firewall_policies')
+def get_all_policy():
+    token = json.loads(request.args.get('token'))
+    policy_json = get_all_policies(token['id'])
+    return json.dumps(policy_json)
+
+
+@main.route('/firewall/<firewall_id>')
+def get_firewall_info(firewall_id):
+    token = json.loads(request.args.get('token'))
+    firewall_info_json = get_one_firewalls_info(token['id'], firewall_id)
+    return json.dumps(firewall_info_json)
+
+
+@main.route('/ports')
+def get_ports_info():
+    token = json.loads(request.args.get('token'))
+    ports_info_json = get_port_all_info(token['id'])
+    return json.dumps(ports_info_json)
+
+
+@main.route('/policy')
+def get_policy_info():
+    token = json.loads(request.args.get('token'))
+    policy_info_json = get_all_policies(token['id'])
+    return json.dumps(policy_info_json)
+
+
+@main.route('/rules')
+def get_rules_info():
+    token = json.loads(request.args.get('token'))
+    rules_info = get_all_rules(token['id'])
+    return json.dumps(rules_info)
+
+
+@main.route('/monitor/<instance_id>/cpu_util')
+def get_monitor_cpu_util(instance_id):
+    token = json.loads(request.args.get('token'))
+    cpu_util_info = get_list_meter_minute(token['id'],'cpu_util',instance_id)
+    return json.dumps(cpu_util_info)
+
+
+@main.route('/monitor/<instance_id>/<meter_name>/<type>')
+def get_monitor_network(instance_id,meter_name, type):
+    token = json.loads(request.args.get('token'))
+    r = get_meter_func_data(token['id'],instance_id, meter_name,type)
+    # monitor_network_outgoing = get_list_meter_minute(token['id'],"network.outgoing.bytes.rate",resource_id)
+    # r = {"monitor_network_incoming":monitor_network_incoming,"monitor_network_outgoing":monitor_network_outgoing}
+    return json.dumps(r)
+
+
+@main.route('/monitor/<instance_id>/disk_info')
+def get_disk_info(instance_id):
+    token = json.loads(request.args.get('token'))
+    disk_read_rate = get_list_meter_minute(token['id'],"disk.read.bytes.rate",instance_id)
+    disk_write_rate = get_list_meter_minute(token['id'],"disk.write.bytes.rate",instance_id)
+    r = dict(disk_read_rate, **disk_write_rate)
+    return json.dumps(r)
+
+
+@main.route('/tuopu')
+def get_tuopu():
+    token = json.loads(request.args.get('token'))
+    r = get_tuopu_info(token['id'], token["tenant"]["id"])
+    return json.dumps(r)
