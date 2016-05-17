25af43a1d9770606fc0810c8b3006ae98cccbc7e
xiaoqiangcoding <352115441@qq.com>
2016-05-17 10:41:02 +0800

    完善了端口的功能

--- collect/monitor.py
+++ collect/monitor.py
@@ -52,4 +52,4 @@ if __name__ == "__main__":
     # print json.dumps(get_meter_func_data(token_id, "f84d2a28-2bef-4075-94c3-de8ec36e52a4", "memory.usage", "minute"))
     # print json.dumps(get_tenant_instances(token_id, tenant_id))
     # print json.dumps(get_server_port(token_id, tenant_id))
-    print delete_subnet(token_id, '{"subnet_ids":["d8f2be93-d772-491d-9827-d30bf20cc2d2"]}')
+    print delete_subnet(token_id, '{"subnet_ids":["d8f2be93-d772-491d-9827-d30bf20cc2d2","195030dc-9bf7-4c4d-a836-627e9ac757f6"]}')
