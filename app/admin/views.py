c65f20b45e71ea0100a5b9ca460432257f660d75
henulwj <henulwj@qq.com>
2017-01-12 10:51:10 +0800

    增加超级管理员登录api

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,3 +1,17 @@
 # _*_ coding:utf-8 _*_
 
+from Flask import request
+import json
+
+from osapi import auth_is_available, get_admin_token
+
 from . import admin
+
+
+@admin.route('/login', methods=['POST'])
+def login():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    token_json = get_admin_token(username, password)
+    return json.dumps(token_json)
+
