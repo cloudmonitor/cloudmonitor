8b1c6b43f4813e7489e2bd90a30b36184db9b0f5
xiaoqiangcoding <352115441@qq.com>
2017-01-13 16:25:44 +0800

    完善摘要信息，添加关于租户、物理主机等数量信息

--- app/admin/views.py
+++ app/admin/views.py
@@ -16,3 +16,16 @@ def login():
     token_json = get_admin_token(username, password)
     return json.dumps(token_json)
 
+
+@admin.route('/abstract/<token_id>/<tenant_id>/<start_time>/<stop_time>')
+def abstract(token_id , start_time, stop_time, tenant_id):
+    """获取概要信息"""
+    abstract_info = get_tenant_usage(token_id, start_time, stop_time, tenant_id)
+    return json.dumps(abstract_info)
+
+
+@admin.route('/hypervisor/<token_id>/<tenant_id>')
+def hypervisor(token_id , tenant_id):
+    """获取物理主机的信息"""
+    hypervisor_info = get_physical_usage(token_id, tenant_id)
+    return json.dumps(hypervisor_info)
